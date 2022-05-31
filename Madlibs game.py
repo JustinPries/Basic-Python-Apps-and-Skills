ec0ee206e4fbd9b36e45cc1c272b035198c57220
JustinPries <34385785+JustinPries@users.noreply.github.com>
2022-05-31 14:00:50 -0400

    Add files via upload

---
+++ Madlibs game.py
@@ -0,0 +1,24 @@
+# MadLibs - Practice Input and Output
+#
+# Template:
+#    I enjoy practice! I find it helps me to __(verb)__ better.
+#    Without practice, my __(noun)__ would probably not even work.
+#    My code is getting more __(adjective)__ every single day!
+
+
+# TODO: Prompt the user for parts of speech and store them in variables
+
+
+
+# TODO: Output the template to the screen with the blanks filled out with what the user stated
+
+print("Hello and Welcome to the Silly Madlibs Game")
+
+verb = input("Please enter a verb!    ")
+noun = input("Please enter a noun!    ")
+adjective = input("Please enter an adjective!    ")
+
+
+print ("I enjoy practice! I find it helps me to", verb, "better. " "Without practice, my", noun, "would probably not even work" )
+print("My code is getting more", adjective,"every single day!!")
+
